1fbdff225418dd1c98e7ca92fd8198fc74a8cfc1
Panterka0804 <Darya.Smirnova0804@yandex.ru>
2024-11-25 19:22:45 +0300

    Add project with LoginWindow and CalculateWindow

---
+++ SocialNetwork/Applications.Desktop.AdminPanel/CalculateWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Applications.Desktop.AdminPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для CalculateWindow.xaml
+    /// </summary>
+    public partial class CalculateWindow : Window
+    {
+        public CalculateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            var ex = mainField.Text;
+            DataTable dt = new DataTable();
+            var result = dt.Compute(ex, string.Empty).ToString();
+            mainField.Text = result;
+        }
+
+
+        private void AddValue1(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "1";
+        }
+
+        private void AddValue2(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "2";
+        }
+        private void AddValue3(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "3";
+        }
+        private void AddValue4(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "4";
+        }
+        private void AddValue5(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "5";
+        }
+        private void AddValue6(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "6";
+        }
+        private void AddValue7(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "7";
+        }
+        private void AddValue8(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "8";
+        }
+        private void AddValue9(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "9";
+        }
+        private void AddValue0(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "0";
+        }
+        private void AddValueUmn(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "*";
+        }
+        private void AddValueDel(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "/";
+        }
+        private void AddValuePlus(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "+";
+        }
+        private void AddValueMinus(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = mainField.Text + "-";
+        }
+        
+        private void AddValueReset(object sender, RoutedEventArgs e)
+        {
+            mainField.Text = "";
+        }
+    }
+      
+
+}
---
+++ SocialNetwork/Applications.Desktop.AdminPanel/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Applications.Desktop.AdminPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string username = Login.Text;
+            string password = Password.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                var window = new CalculateWindow();
+                window.Title = "КАЛЬКУЛЯТОР";
+                window.Owner = this;
+                window.Show();
+            }
+            else if (username == null || password == null) 
+            {
+                Login.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                MessageBox.Show("Введите логин и пароль!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            else if (username != "admin" && password != "admin")
+            {
+                Login.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                MessageBox.Show("Проверьте правильность ввода логина и пароля!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+        }
+    }
+}
